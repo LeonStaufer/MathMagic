d1bfc3dd21d5f3c5ca10aabfc003da199995c9fb
LeonStaufer <leon@staufer.me>
2020-03-26 14:38:37 +0100

    fixed initial render

--- mathmagic/src/main/java/me/staufer/mathmagic/MathView.java
+++ mathmagic/src/main/java/me/staufer/mathmagic/MathView.java
@@ -6,54 +6,80 @@ import android.text.Html;
 import android.util.AttributeSet;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import org.apache.commons.text.StringEscapeUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * MathView class that can render TeX using KaTeX in WebView
+ */
 public class MathView extends WebView {
     private final JSONObject options;
 
+    /**
+     * constructor for creating a MathView
+     *
+     * @param context Context of View
+     * @param attrs   AttributeSet
+     */
     @SuppressLint("SetJavaScriptEnabled")
     public MathView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
+        //enable JavaScript
         WebSettings webSettings = this.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        //connect MathViewInterface to MathView
         this.addJavascriptInterface(new MathViewInterface(context), "MathViewInterface");
-        this.loadUrl("file:///android_asset/index.html");
 
+        //create JSON options object
+        //TODO take options from attrs
         options = new JSONObject();
         try {
             options.put("throwOnError", false);
             options.put("displayMode", true);
-            options.put("color", "white");
-            options.put("background", "black");
-            //TODO fix newlines https://katex.org/docs/options.html, see trust
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        final MathView mathView = this;
+        //default URI
+        URI uri = URI.create("file:///android_asset/index.html");
+        try {
+            //URI with options
+            uri = new URI("file", null, "///android_asset/index.html", "options=" + options.toString(), null);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
 
-        this.setWebViewClient(new WebViewClient(){
-            @Override
-            public void onPageFinished(WebView view, String url){
-                //TODO might cause flash of unstyled document
-                mathView.loadUrl("javascript:init("+options.toString() + ")");
-            }
-        });
+        //load HTML page with KaTeX
+        this.loadUrl(uri.toString());
     }
 
+    /**
+     * render a TeX string in the MathView
+     *
+     * @param tex string using the TeX format
+     */
     public void render(String tex) {
         this.render(tex, this.options);
     }
 
+    /**
+     * render a TeX string in the MathView with the given options
+     *
+     * @param tex     string using the TeX format
+     * @param options JSONObject with options for MathView and KaTeX
+     */
     public void render(String tex, JSONObject options) {
+        //sanitize input
         tex = Html.escapeHtml(tex);
         tex = StringEscapeUtils.escapeEcmaScript(tex);
+        //call JS update function with tex and options
         this.loadUrl("javascript:update('" + tex + "'," + options.toString() + ")");
     }
 }
--- mathmagic/src/main/java/me/staufer/mathmagic/MathViewInterface.java
+++ mathmagic/src/main/java/me/staufer/mathmagic/MathViewInterface.java
@@ -7,19 +7,33 @@ import android.webkit.JavascriptInterface;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Javascript Interface for MathView
+ */
 public class MathViewInterface {
     private Context context;
     private int height;
 
+    /**
+     * MathViewInterface constructor that takes context of the interface
+     * @param c Context
+     */
     public MathViewInterface(Context c) {
         context = c;
     }
 
+    /**
+     * @param height
+     */
     @JavascriptInterface
     public void updateHeight(int height){
         this.height = height;
     }
 
+    /**
+     * JavaScript interface that allows Android logging
+     * @param msg to be logged
+     */
     @JavascriptInterface
     public void log(String msg){
         Log.i("MathViewInterface", msg);
