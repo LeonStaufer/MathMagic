70c003da4306788f99c2a4074dfaeddd70588fde
LeonStaufer <leon@staufer.me>
2020-03-26 14:38:51 +0100

    improved example

--- app/src/main/java/me/staufer/math_example/MainActivity.java
+++ app/src/main/java/me/staufer/math_example/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+/**
+ * Basic example to show how MathView works
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,13 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //get MathView Elemenet
         final MathView mathView = findViewById(R.id.mathview);
         mathView.setWebViewClient(new WebViewClient(){
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
 
-                mathView.render("x \\times x = x^2");
+                //render TeX once MathView has loaded
+                //backslashes need to be escaped
+                mathView.render("x \\times x = x^2 \\ \\ \\text{Example}");
             }
         });
     }
