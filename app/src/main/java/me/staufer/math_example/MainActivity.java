e7ce5ca3d250099227293f505bbedee532a2e603
LeonStaufer <leon@staufer.me>
2020-03-27 12:32:35 +0100

    extended example with interactive input

--- app/src/main/java/me/staufer/math_example/MainActivity.java
+++ app/src/main/java/me/staufer/math_example/MainActivity.java
@@ -1,11 +1,14 @@
 package me.staufer.math_example;
 
-import androidx.appcompat.app.AppCompatActivity;
-import me.staufer.mathmagic.MathView;
-
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import me.staufer.mathmagic.MathView;
 
 /**
  * Basic example to show how MathView works
@@ -17,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //get MathView Elemenet
+        //get MathView component
         final MathView mathView = findViewById(R.id.mathview);
-        mathView.setWebViewClient(new WebViewClient(){
+        mathView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
@@ -27,6 +30,26 @@ public class MainActivity extends AppCompatActivity {
                 //render TeX once MathView has loaded
                 //backslashes need to be escaped
                 mathView.render("x \\times x = x^2 \\\\ \\text{Example in Java}");
+            }
+        });
+
+        //get components for interactive MathView
+        final MathView mathViewInteractive = findViewById(R.id.mathview_interactive);
+        final EditText input = findViewById(R.id.input);
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mathViewInteractive.render(s.toString());
             }
         });
     }
